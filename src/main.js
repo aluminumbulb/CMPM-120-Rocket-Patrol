83a9265bfe433d7c50526ee8e8656f1cfe51faa9
“Adrian <adrmille@ucsc.edu>
2021-04-08 16:44:41 -0700

    got blank black screen!

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
-const { Phaser } = require("../lib/phaser");
-
-let config = {
-    type: Phaser.CANVAS,
-    width: 640,
-    height: 480,
-    scene: [Menu],
-    scene: [Play]
-}
+ let config = {
+//     type: Phaser.CANVAS,
+     width: 640,
+     height: 480,
+//     scene: [Menu],
+//     scene: [Play]
+ }
 
 let game = new Phaser.Game(config);
 
-let borderUISize;
-let borderPadding;
+//let borderUISize;
+//let borderPadding;
