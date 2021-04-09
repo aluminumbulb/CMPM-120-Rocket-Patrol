2af190de33c98124f6fb0167111a3366b9d693fe
“Adrian <adrmille@ucsc.edu>
2021-04-08 19:56:32 -0700

    added assets

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,8 +3,17 @@ class Play extends Phaser.Scene{
         super("playScene");
         console.log("playscene created");
     }
+    // phaser contained function
+    preload(){
+        this.load.image('starfield','assets/starfield.png');
+    }
 
     create(){
+        //layers are important
+        this.starfield = this.add.tileSprite(
+            0,0,640,480, 'starfield'
+        ).setOrigin(0,0);
+
         //this.add.text(20,20,"Playing Scene");
         //green bar
         this.add.rectangle(0,borderUISize+borderPadding, game.config.width, borderUISize *2, 0x00FF00).setOrigin(0,0);
