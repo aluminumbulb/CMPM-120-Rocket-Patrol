d6c2310cc864fc35da6c6182243c13bdfc50a48b
“Adrian <adrmille@ucsc.edu>
2021-04-11 18:22:11 -0700

    finished class work

--- src/prefabs/Ships.js
+++ src/prefabs/Ships.js
@@ -10,4 +10,9 @@ class Ships extends Phaser.GameObjects.Sprite{
             this.x = game.config.width;
         }
     }
+
+    reset(){
+        this.x = game.config.width + 50;
+        this.alpha = 1;
+    }
 }
