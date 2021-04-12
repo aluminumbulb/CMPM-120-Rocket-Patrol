29e337517e53f884c2ebbd21d92283e92a261ae2
“Adrian <adrmille@ucsc.edu>
2021-04-11 20:06:47 -0700

    janky menu set up

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,6 +5,7 @@ class Rocket extends Phaser.GameObjects.Sprite{
         scene.add.existing(this);
         this.movementSpeed = 2;
         this.isFiring = false;
+        this.sfxRocket = scene.sound.add('sfx_rocket');
     }
     
     update(){
@@ -23,6 +24,7 @@ class Rocket extends Phaser.GameObjects.Sprite{
             }
             if(Phaser.Input.Keyboard.JustDown(keyF)){
                 this.isFiring = true;
+                this.sfxRocket.play();
             }
         }
         this.x = Phaser.Math.Clamp(this.x, borderUISize+borderPadding, game.config.width-borderUISize-borderPadding)
